a107825962e44ab24389d428b89ebc659b5debf6
alhamami <jalalalhamami@gmail.com>
2024-12-21 17:14:07 +0300

    Added User model

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/User.java
@@ -0,0 +1,60 @@
+package com.udacity.jwdnd.course1.cloudstorage.model;
+
+public class User {
+
+    private Integer userid;
+    private String username;
+    private String salt;
+    private String password;
+
+    public Integer getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Integer userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    private String firstName;
+    private String lastName;
+}
