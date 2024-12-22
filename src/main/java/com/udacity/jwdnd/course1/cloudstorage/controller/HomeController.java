31f2b4394bda3f1ca5f1f1ac18b485afa96ad10b
alhamami <jalalalhamami@gmail.com>
2024-12-22 22:18:54 +0300

    Updated home controller

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -1,18 +1,12 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
 import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
+import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import com.udacity.jwdnd.course1.cloudstorage.model.File;
-import java.io.IOException;
-import java.io.InputStream;
 
 @Controller
 @RequestMapping("/home")
@@ -20,15 +14,19 @@ public class HomeController {
 
 
     private final FileService fileService;
+    private final NoteService noteService;
 
-    public HomeController(FileService fileService) {
+    public HomeController(FileService fileService, NoteService noteService) {
         this.fileService = fileService;
+        this.noteService = noteService;
     }
 
     @GetMapping()
-    public String home(Authentication authentication, Model model){
+    public String home(@ModelAttribute("saveNote") Note saveNote, Authentication authentication, Model model){
 
+        model.addAttribute("notes", noteService.getAllNotes(authentication.getName()));
         model.addAttribute("files", fileService.getAllFiles(authentication.getName()));
+
         return "home";
     }
 
