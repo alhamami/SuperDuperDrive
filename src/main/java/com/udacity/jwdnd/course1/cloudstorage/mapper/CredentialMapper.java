58bc364968617e3ff146e9e3beb539e2c5b1b7dd
alhamami <jalalalhamami@gmail.com>
2024-12-23 16:22:24 +0300

    Implemented CredentialMapper for Credential

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/CredentialMapper.java
@@ -0,0 +1,26 @@
+package com.udacity.jwdnd.course1.cloudstorage.mapper;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
+import org.apache.ibatis.annotations.*;
+import com.udacity.jwdnd.course1.cloudstorage.model.Credential;
+
+import java.util.List;
+
+@Mapper
+public interface CredentialMapper {
+
+    @Insert("INSERT INTO CREDENTIALS (url, username, password, userid) VALUES (#{url}, #{username}, #{password}, #{userid})")
+    @Options(useGeneratedKeys = true, keyProperty = "credentialid")
+    int saveCredential(Credential credential);
+
+    @Select("SELECT c.* FROM CREDENTIALS c JOIN USERS u ON c.userid = u.userid WHERE u.username = #{username}")
+    List<Credential> getAllCredentials(String username);
+
+    @Delete("DELETE FROM CREDENTIALS c USING USERS u WHERE c.credentialid = #{credentialid} AND u.username = #{username}")
+    int deleteCredential(Integer credentialid, String username);
+
+    @Update("UPDATE CREDENTIALS SET url = #{url}, username = #{username}, password = #{password} WHERE credentialid = #{credentialid}")
+    int editCredential(Credential credential);
+
+
+}
