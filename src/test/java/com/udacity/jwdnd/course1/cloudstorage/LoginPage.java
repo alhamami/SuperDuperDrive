e70a485877b7035ce216d605da8352724b7c888a
alhamami <jalalalhamami@gmail.com>
2024-12-24 03:03:47 +0300

    Created LoginPage

---
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/LoginPage.java
@@ -0,0 +1,25 @@
+package com.udacity.jwdnd.course1.cloudstorage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    @FindBy(id="inputUsername")
+    private WebElement usernameField;
+
+    @FindBy(id="inputPassword")
+    private WebElement passwordField;
+
+    @FindBy(id="login-button")
+    private WebElement submitButton;
+
+    public LoginPage(WebDriver webDriver) {
+
+        PageFactory.initElements(webDriver, this);
+    }
+
+}
